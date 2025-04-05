f65daffcbd38d305c79cc855a76e9af9f7da3cb3
AntonProl <antonprolperez@gmail.com>
2025-04-05 12:17:32 +0200

    PIOT-GDA-07-001 completo con tests funcionando.

--- src/main/java/programmingtheiot/gda/connection/MqttClientConnector.java
+++ src/main/java/programmingtheiot/gda/connection/MqttClientConnector.java
@@ -8,7 +8,6 @@
 
 package programmingtheiot.gda.connection;
 
-import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,8 +17,6 @@ import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
-import org.eclipse.paho.client.mqttv3.MqttPersistenceException;
-import org.eclipse.paho.client.mqttv3.MqttSecurityException;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 import programmingtheiot.common.ConfigConst;
@@ -39,7 +36,20 @@ public class MqttClientConnector implements IPubSubClient, MqttCallbackExtended
 		Logger.getLogger(MqttClientConnector.class.getName());
 	
 	// params
-	
+	private boolean useAsyncClient = false;
+
+	private MqttClient           mqttClient = null;
+	private MqttConnectOptions   connOpts = null;
+	private MemoryPersistence    persistence = null;
+	private IDataMessageListener dataMsgListener = null;
+
+	private String  clientID = null;
+	private String  brokerAddr = null;
+	private String  host = ConfigConst.DEFAULT_HOST;
+	private String  protocol = ConfigConst.DEFAULT_MQTT_PROTOCOL;
+	private int     port = ConfigConst.DEFAULT_MQTT_PORT;
+	private int     brokerKeepAlive = ConfigConst.DEFAULT_KEEP_ALIVE;
+
 	
 	// constructors
 	
@@ -50,6 +60,58 @@ public class MqttClientConnector implements IPubSubClient, MqttCallbackExtended
 	public MqttClientConnector()
 	{
 		super();
+		ConfigUtil configUtil = ConfigUtil.getInstance();
+
+		this.host =
+	    	configUtil.getProperty(
+	        	ConfigConst.MQTT_GATEWAY_SERVICE, ConfigConst.HOST_KEY, ConfigConst.DEFAULT_HOST);
+
+		this.port =
+	    	configUtil.getInteger(
+	        	ConfigConst.MQTT_GATEWAY_SERVICE, ConfigConst.PORT_KEY, ConfigConst.DEFAULT_MQTT_PORT);
+
+		this.brokerKeepAlive =
+	    	configUtil.getInteger(
+	        	ConfigConst.MQTT_GATEWAY_SERVICE, ConfigConst.KEEP_ALIVE_KEY, ConfigConst.DEFAULT_KEEP_ALIVE);
+
+	// Esta siguiente propiedad booleana del archivo de configuración es opcional; puede
+	// establecerse dentro de las secciones [Mqtt.GatewayService] y [Cloud.GatewayService]
+	// de PiotConfig.props. Puedes usarla para crear un flujo lógico dentro de esta clase
+	// para determinar si usar MqttClient o MqttAsyncClient, o simplemente elegir una de
+	// las dos clases según tus necesidades de uso. En general, MqttAsyncClient será
+	// necesario al ejecutar el GDA como una aplicación, ya que necesitará manejar mensajes
+	// entrantes y salientes usando MQTT simultáneamente. Para pruebas solo del GDA usando
+	// los casos de prueba especificados en este módulo de laboratorio y otros, generalmente
+	// es mejor - y probablemente requerido - usar MqttClient.
+	//
+	// IMPORTANTE: Si estás usando una versión antigua de ConfigConst.java,
+	// necesitarás agregar la siguiente línea de código a ConfigConst.java:
+	// public static final String USE_ASYNC_CLIENT_KEY = "useAsyncClient";
+		this.useAsyncClient =
+	    	configUtil.getBoolean(
+	        	ConfigConst.MQTT_GATEWAY_SERVICE, ConfigConst.USE_ASYNC_CLIENT_KEY);
+
+	// NOTA: el cliente Java paho requiere un client ID - por ahora,
+	// puedes usar el client ID generado; para ejercicios posteriores,
+	// deberías definir uno propio y cargarlo desde el archivo de configuración
+		this.clientID = MqttClient.generateClientId();
+
+	// estos son específicos para la conexión MQTT que se usará durante el connect
+		this.persistence = new MemoryPersistence();
+		this.connOpts = new MqttConnectOptions();
+
+		this.connOpts.setKeepAliveInterval(this.brokerKeepAlive);
+
+	// NOTA: Si se usa un clientID aleatorio para cada nueva conexión,
+	// la sesión limpia debe estar en 'true'; ver especificación MQTT para más detalles
+		this.connOpts.setCleanSession(false);
+
+	// NOTA: La reconexión automática puede ser una función útil para recuperación de conexión
+		this.connOpts.setAutomaticReconnect(true);
+
+	// NOTA: La URL no tiene un manejador de protocolo para "tcp",
+	// así que necesitamos construir la URL manualmente
+		this.brokerAddr = this.protocol + "://" + this.host + ":" + this.port;
 	}
 	
 	
@@ -58,12 +120,44 @@ public class MqttClientConnector implements IPubSubClient, MqttCallbackExtended
 	@Override
 	public boolean connectClient()
 	{
+		try {
+			if (this.mqttClient == null) {
+				this.mqttClient = new MqttClient(this.brokerAddr, this.clientID, this.persistence);
+				this.mqttClient.setCallback(this);
+			}
+	
+			if (! this.mqttClient.isConnected()) {
+				_Logger.info("MQTT client connecting to broker: " + this.brokerAddr);
+				this.mqttClient.connect(this.connOpts);
+				return true;
+			} else {
+				_Logger.warning("MQTT client already connected to broker: " + this.brokerAddr);
+			}
+		} catch (MqttException e) {
+			// TODO: manejar esta excepción
+			_Logger.log(Level.SEVERE, "Failed to connect MQTT client to broker.", e);
+		}
 		return false;
 	}
 
 	@Override
 	public boolean disconnectClient()
 	{
+		
+		try {
+			if (this.mqttClient != null) {
+				if (this.mqttClient.isConnected()) {
+					_Logger.info("Disconnecting MQTT client from broker: " + this.brokerAddr);
+					this.mqttClient.disconnect();
+					return true;
+				} else {
+					_Logger.warning("MQTT client not connected to broker: " + this.brokerAddr);
+				}
+			}
+		} catch (Exception e) {
+		// TODO: manejar esta excepción
+			_Logger.log(Level.SEVERE, "Failed to disconnect MQTT client from broker: " + this.brokerAddr, e);
+	}
 		return false;
 	}
 
@@ -99,6 +193,10 @@ public class MqttClientConnector implements IPubSubClient, MqttCallbackExtended
 	@Override
 	public boolean setDataMessageListener(IDataMessageListener listener)
 	{
+		if (listener != null) {
+		this.dataMsgListener = listener;
+		return true;
+	}
 		return false;
 	}
 	
